c7b951e13ba081482811b788c32ceaef4d3077aa
MagicznyMaciej <emem1928@gmail.com>
2019-04-25 20:59:25 +0200

    New models for infrastructure

--- YourHome.Infrastructure/Repositories/OfferRepository.cs
+++ YourHome.Infrastructure/Repositories/OfferRepository.cs
@@ -1,28 +1,34 @@
-﻿using Nest;
+﻿using AutoMapper;
+using Nest;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using YourHome.Core.Models.Domain;
+using YourHome.Infrastructure.Models;
 using YourHome.Core.RepositoryInterfaces;
+using Offer = YourHome.Infrastructure.Models.Offer;
 
 namespace YourHome.Infrastructure.Repositories
 {
     public class OfferRepository : IOfferRepository
     {
         private readonly IElasticClient _elasticClient;
+        private readonly IMapper _mapper;
 
-        public OfferRepository(IElasticClient elasticClient)
+        public OfferRepository(IElasticClient elasticClient, IMapper mapper)
         {
             _elasticClient = elasticClient;
+            _mapper = mapper;
         }
 
-        public Offer Get(string id)
+        public Core.Models.Domain.Offer Get(string id)
         {
             var response = _elasticClient.Get<Offer>(id);
-            return response.Source;
+            var offer = _mapper.Map<Core.Models.Domain.Offer>(response.Source);
+            return offer;
         }
 
-        public void Add(Offer offer)
+        public void Add(Core.Models.Domain.Offer offer)
         {
             _elasticClient.Index(offer, i => i);
         }
