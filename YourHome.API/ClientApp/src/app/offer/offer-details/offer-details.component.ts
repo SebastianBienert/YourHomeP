0bd70af7284315bdc50715b0b9a9d093c16de629
Mateusz_Thomas <mateuszthomas1@gmail.com>
2019-04-28 17:42:29 +0200

    Changed photos property binding

--- YourHome.API/ClientApp/src/app/offer/offer-details/offer-details.component.ts
+++ YourHome.API/ClientApp/src/app/offer/offer-details/offer-details.component.ts
@@ -18,15 +18,10 @@ export class OfferDetailsComponent implements OnInit {
 
   offer: Offer;
 
-  urls: string[];
-
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('offerId');
     this.offerService.get(id).subscribe(o => {
       this.offer = o;
-      this.urls = o.images;
     });
-    //this.urls = ["https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"];
   }
-
 }
